6d3e218dd10d7876056033b5a40d281178e854d0
HM22-code <H.M-22@outlook.fr>
2024-10-18 18:44:02 +0200

    Adding Form

--- src/components/Booking.vue
+++ src/components/Booking.vue
@@ -1,60 +1,74 @@
-<template lang="">
+<template>
     <div class="container">
-        <!-- Content here -->
         <form action="mailto:contact@aubergepeillon.fr" enctype="text/plain" method="post">
-			<fieldset>
-				<label for="monsieur">Monsieur</label>
-				<input type="radio" id="monsieur" name="genre" value="Monsieur">
-				<label for="madame">Madame</label>
-				<input type="radio" id="madame" name="genre" value="madame">
-				<p>
-					<label for="nom">Nom</label>
-					<input type="text" id="nom" required  name="nom" >
-					<label for="prenom">Prénom</label>
-					<input type="text" id="prenom" required name="prenom" >
-				</p>
-				<p>
-					<label for="mail">Mail</label>
-					<input type="email" id="mail" required name="mail" >
-					<label for="tel">Téléphone</label>
-					<input type="tel" id="tel" name="telephone" placeholder="Sans points,ni espaces" required>
-				</p>
-				<select id="chambre" name="Chambre">
+			<div class="form-check form-check-inline">
+				<input class="form-check-input" type="radio" id="monsieur" name="genre" value="Monsieur">
+				<label class="form-check-label" for="monsieur">Monsieur</label>
+			</div>
+			<div class="form-check form-check-inline">
+				<input class="form-check-input" type="radio" id="madame" name="genre" value="madame">
+				<label class="form-check-label" for="madame">Madame</label>
+			</div>
+			<div class="mb-3">
+				<label class="form-label" for="nom">Nom</label>
+				<input class="form-control" type="text" id="nom" required  name="nom" >
+			</div>
+			<div class="mb-3">
+				<label class="form-label" for="prenom">Prénom</label>
+				<input class="form-control" type="text" id="prenom" required name="prenom" >
+			</div>
+			<div class="mb-3">
+				<label class="form-label" for="mail">Mail</label>
+				<input class="form-control" type="email" id="mail" required name="mail" >
+			</div>
+			<div class="mb-3">
+				<label class="form-label" for="tel">Téléphone</label>
+				<input class="form-control" type="tel" id="tel" name="telephone" placeholder="Sans points,ni espaces" required>
+			</div>
+			<div class="mb-3">
+				<label class="form-label" for="chambre">Chambre</label>
+				<select class="form-select" id="chambre" name="Chambre">
 					<option value="La Calade">La Calade</option>
 					<option value="les Lucioles">Les Lucioles</option>
 					<option value="Les Myrtes">Les Myrtes</option>
 					<option value="La Fontaine">La Fontaine</option>
 					<option value="Les Oliviers">Les Oliviers</option>
 				</select>
-				<p>
-					<label for="datearrive">Date d'arrivée</label>
-					<input type="date" id="datearrive" required name="date d'arrivé" >
-					<label for="datedepart">Date de départ</label>
-					<input type="date" id="datedepart" required name="date de départ" >
-				</p>
-				<p>
-					<label for="npersonne">Nombre de personnes</label>
-					<input type="number" id="npersonne" name="nombre de personnes" min="1" max="4">
-				</p>
-				<p>
-					<label for="npetitdejeuner">Nombre de petits déjeuners</label>
-					<input type="number" id="npetitdejeuner" name="nombre de petits déjeuners" min="0" max="4">
-				</p>
-				<p>
-					<label for="parking">Parking</label>
-					<input type="checkbox" id="parking" name="parking">
-				</p>
-				<p>Commentaires:</p>
-				<textarea>placeholder="Vous pouvez nous laisser ici un message"</textarea>
-				<p>
-				<label for="confidentialite"></label>
-				<input type="checkbox" id="confidentialite" required name=" confidentialité">
-				En cochant cette case,vous acceptez notre <a href="confidentialite.html">politique de confidentialité</a>
-				</p>
-				<p>
-					<input type="submit" value="Valider">
-				</p>
-			</fieldset>
+			</div>
+			<div class="mb-3">
+				<label class="form-label" for="datearrive">Date d'arrivée</label>
+				<input class="form-control" type="date" id="datearrive" required name="date d'arrivé" >
+			</div>
+			<div class="mb-3">
+				<label class="form-label" for="datedepart">Date de départ</label>
+				<input class="form-control" type="date" id="datedepart" required name="date de départ" >
+			</div>
+			<div class="mb-3">
+				<label class="form-label" for="npersonne">Nombre de personnes</label>
+				<input class="form-control" type="number" id="npersonne" name="nombre de personnes" min="1" max="4">
+			</div>
+			<div class="mb-3">
+				<label class="form-label" for="npetitdejeuner">Nombre de petits déjeuners</label>
+				<input class="form-control" type="number" id="npetitdejeuner" name="nombre de petits déjeuners" min="0" max="4">
+			</div>
+			<div class="mb-3 form-check">
+				<input class="form-check-input" type="checkbox" id="parking" name="parking">
+				<label class="form-check-label" for="parking">Parking</label>
+			</div>
+			<div class="mb-3">
+				<label class="form-label" for="commentaires">Message</label>
+				<textarea class="form-control" id="commentaires" placeholder="Vous pouvez nous laisser ici un message"></textarea>
+			</div>
+			<div class="mb-3 form-check">
+				<input class="form-check-input" type="checkbox" id="confidentialite" required name=" confidentialité" />
+				<label class="form-check-label" for="confidentialite">
+					En cochant cette case,vous acceptez notre
+					<router-link to="/confidentiality">Politique de confidentialité</router-link>
+				</label>
+			</div>
+			<div class="mb-3">
+				<input class="btn btn-primary" type="submit" value="Valider" />
+			</div>
 		</form>
     </div>
 </template>
