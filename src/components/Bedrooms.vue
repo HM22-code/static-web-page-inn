f1a4c631e97d15bc8fa3e27ab4aabdb3074b7442
HM22-code <H.M-22@outlook.fr>
2024-10-18 18:21:48 +0200

    Adding Table and Carousel

--- src/components/Bedrooms.vue
+++ src/components/Bedrooms.vue
@@ -1,42 +1,132 @@
-<template lang="">
+<template>
     <div class="container">
-        <!-- Content here -->
-        <table>
-            <tr>
-                <th>Nom</th>
-                <th>Type de chambre</th>
-                <th>Prix par nuit</th>
-            </tr>
-            <tr>
-                <td>La Calade</td>
-                <td class="colonnedeux">Chambre simple (1 pers)</td>
-                <td class="colonnetrois">65</td>
-            </tr>
-            <tr>
-                <td>Les Lucioles</td>
-                <td class="colonnedeux">Chambre double (2 lits)</td>
-                <td class="colonnetrois">95</td>
-            </tr>
-            <tr>
-                <td>Les Myrtes</td>
-                <td class="colonnedeux" rowspan="2">Chambre double (1 lit)</td>
-                <td class="colonnetrois" rowspan="2">95</td>
-            </tr>
-            <tr>
-                <td>La Fontaine</td>
-            </tr>
-            <tr>
-                <td>Les Oliviers</td>
-                <td class="colonnedeux">Familiale (1 gd lit + 2 petits)</td>
-                <td class="colonnetrois">135</td>
-            </tr>
+        <div id="carousel" class="carousel slide">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                <button type="button" data-bs-target="#carousel" data-bs-slide-to="4" aria-label="Slide 5"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="../assets/calade.jpg" class="d-block w-100" alt="chambre">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>La Calade</h5>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="../assets/lucioles.jpg" class="d-block w-100" alt="chambre">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Les Lucioles</h5>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="../assets/myrtes.jpg" class="d-block w-100" alt="chambre">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Les Myrtes</h5>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="../assets/fontaine.jpg" class="d-block w-100" alt="chambre">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>La Fontaine</h5>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="../assets/oliviers.jpg" class="d-block w-100" alt="chambre">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Les Oliviers</h5>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Nom</th>
+                    <th scope="col">Type de chambre</th>
+                    <th scope="col">Prix par nuit</th>
+                </tr>
+            </thead>
+            <tbody class="table-group-divider">
+                <tr id="calade">
+                    <td scope="row">La Calade</td>
+                    <td>Chambre simple (1 pers)</td>
+                    <td>65 €</td>
+                </tr>
+                <tr id="lucioles">
+                    <td scope="row">Les Lucioles</td>
+                    <td>Chambre double (2 lits)</td>
+                    <td>95 €</td>
+                </tr>
+                <tr id="myrtes">
+                    <td scope="row">Les Myrtes</td>
+                    <td>Chambre double (1 lit)</td>
+                    <td>95 €</td>
+                </tr>
+                <tr id="fontaine">
+                    <td scope="row">La Fontaine</td>
+                    <td>Chambre double (1 lit)</td>
+                    <td>95 €</td>
+                </tr>
+                <tr id="oliviers">
+                    <td>Les Oliviers</td>
+                    <td>Familiale (1 gd lit + 2 petits)</td>
+                    <td>135 €</td>
+                </tr>
+            </tbody>
         </table>
     </div>
 </template>
-<script>
-export default {
+<script setup lang="js">
+import { Carousel } from 'bootstrap'
+import { onMounted } from 'vue'
 
-}
+onMounted(() => {
+    const myCarouselElement = document.querySelector('#carousel')
+    const carousel = new Carousel(myCarouselElement)
+    myCarouselElement.addEventListener('slid.bs.carousel', event => {
+        if (event.to == 0) {
+            document.querySelector('#calade').classList.add('table-active')
+        }
+        if (event.to == 1) {
+            document.querySelector('#lucioles').classList.add('table-active')
+        }
+        if (event.to == 2) {
+            document.querySelector('#myrtes').classList.add('table-active')
+        }
+        if (event.to == 3) {
+            document.querySelector('#fontaine').classList.add('table-active')
+        }
+        if (event.to == 4) {
+            document.querySelector('#oliviers').classList.add('table-active')
+        }
+        if (event.from == 0) {
+            document.querySelector('#calade').classList.remove('table-active')
+        }
+        if (event.from == 1) {
+            document.querySelector('#lucioles').classList.remove('table-active')
+        }
+        if (event.from == 2) {
+            document.querySelector('#myrtes').classList.remove('table-active')
+        }
+        if (event.from == 3) {
+            document.querySelector('#fontaine').classList.remove('table-active')
+        }
+        if (event.from == 4) {
+            document.querySelector('#oliviers').classList.remove('table-active')
+        }
+    })
+})
 </script>
 <style lang="">
 
