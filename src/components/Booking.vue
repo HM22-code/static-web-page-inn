5ef863f6d21958014886d42c20e18333d84b74a2
HM22-code <H.M-22@outlook.fr>
2024-10-22 23:21:10 +0200

    Update template and style

--- src/components/Booking.vue
+++ src/components/Booking.vue
@@ -1,75 +1,77 @@
 <template>
     <div class="container">
-        <form action="mailto:contact@aubergepeillon.fr" enctype="text/plain" method="post">
-			<div class="form-check form-check-inline">
-				<input class="form-check-input" type="radio" id="monsieur" name="genre" value="Monsieur">
-				<label class="form-check-label" for="monsieur">Monsieur</label>
-			</div>
-			<div class="form-check form-check-inline">
-				<input class="form-check-input" type="radio" id="madame" name="genre" value="madame">
-				<label class="form-check-label" for="madame">Madame</label>
-			</div>
-			<div class="mb-3">
-				<label class="form-label" for="nom">Nom</label>
-				<input class="form-control" type="text" id="nom" required  name="nom" >
-			</div>
-			<div class="mb-3">
-				<label class="form-label" for="prenom">Prénom</label>
-				<input class="form-control" type="text" id="prenom" required name="prenom" >
-			</div>
-			<div class="mb-3">
-				<label class="form-label" for="mail">Mail</label>
-				<input class="form-control" type="email" id="mail" required name="mail" >
-			</div>
-			<div class="mb-3">
-				<label class="form-label" for="tel">Téléphone</label>
-				<input class="form-control" type="tel" id="tel" name="telephone" placeholder="Sans points,ni espaces" required>
-			</div>
-			<div class="mb-3">
-				<label class="form-label" for="chambre">Chambre</label>
-				<select class="form-select" id="chambre" name="Chambre">
-					<option value="La Calade">La Calade</option>
-					<option value="les Lucioles">Les Lucioles</option>
-					<option value="Les Myrtes">Les Myrtes</option>
-					<option value="La Fontaine">La Fontaine</option>
-					<option value="Les Oliviers">Les Oliviers</option>
-				</select>
-			</div>
-			<div class="mb-3">
-				<label class="form-label" for="datearrive">Date d'arrivée</label>
-				<input class="form-control" type="date" id="datearrive" required name="date d'arrivé" >
-			</div>
-			<div class="mb-3">
-				<label class="form-label" for="datedepart">Date de départ</label>
-				<input class="form-control" type="date" id="datedepart" required name="date de départ" >
-			</div>
-			<div class="mb-3">
-				<label class="form-label" for="npersonne">Nombre de personnes</label>
-				<input class="form-control" type="number" id="npersonne" name="nombre de personnes" min="1" max="4">
-			</div>
-			<div class="mb-3">
-				<label class="form-label" for="npetitdejeuner">Nombre de petits déjeuners</label>
-				<input class="form-control" type="number" id="npetitdejeuner" name="nombre de petits déjeuners" min="0" max="4">
-			</div>
-			<div class="mb-3 form-check">
-				<input class="form-check-input" type="checkbox" id="parking" name="parking">
-				<label class="form-check-label" for="parking">Parking</label>
-			</div>
-			<div class="mb-3">
-				<label class="form-label" for="commentaires">Message</label>
-				<textarea class="form-control" id="commentaires" placeholder="Vous pouvez nous laisser ici un message"></textarea>
-			</div>
-			<div class="mb-3 form-check">
-				<input class="form-check-input" type="checkbox" id="confidentialite" required name=" confidentialité" />
-				<label class="form-check-label" for="confidentialite">
-					En cochant cette case,vous acceptez notre
-					<router-link to="/confidentiality">Politique de confidentialité</router-link>
-				</label>
-			</div>
-			<div class="mb-3">
-				<input class="btn btn-primary" type="submit" value="Valider" />
-			</div>
-		</form>
+		<div class="card m-4 p-4">
+			<form action="mailto:contact@aubergepeillon.fr" enctype="text/plain" method="post">
+				<div class="form-check form-check-inline">
+					<input class="form-check-input" type="radio" id="monsieur" name="genre" value="Monsieur">
+					<label class="form-check-label" for="monsieur">Monsieur</label>
+				</div>
+				<div class="form-check form-check-inline">
+					<input class="form-check-input" type="radio" id="madame" name="genre" value="madame">
+					<label class="form-check-label" for="madame">Madame</label>
+				</div>
+				<div class="mb-3">
+					<label class="form-label" for="nom">Nom</label>
+					<input class="form-control" type="text" id="nom" required  name="nom" >
+				</div>
+				<div class="mb-3">
+					<label class="form-label" for="prenom">Prénom</label>
+					<input class="form-control" type="text" id="prenom" required name="prenom" >
+				</div>
+				<div class="mb-3">
+					<label class="form-label" for="mail">Mail</label>
+					<input class="form-control" type="email" id="mail" required name="mail" >
+				</div>
+				<div class="mb-3">
+					<label class="form-label" for="tel">Téléphone</label>
+					<input class="form-control" type="tel" id="tel" name="telephone" placeholder="Sans points,ni espaces" required>
+				</div>
+				<div class="mb-3">
+					<label class="form-label" for="chambre">Chambre</label>
+					<select class="form-select" id="chambre" name="Chambre">
+						<option value="La Calade">La Calade</option>
+						<option value="les Lucioles">Les Lucioles</option>
+						<option value="Les Myrtes">Les Myrtes</option>
+						<option value="La Fontaine">La Fontaine</option>
+						<option value="Les Oliviers">Les Oliviers</option>
+					</select>
+				</div>
+				<div class="mb-3">
+					<label class="form-label" for="datearrive">Date d'arrivée</label>
+					<input class="form-control" type="date" id="datearrive" required name="date d'arrivé" >
+				</div>
+				<div class="mb-3">
+					<label class="form-label" for="datedepart">Date de départ</label>
+					<input class="form-control" type="date" id="datedepart" required name="date de départ" >
+				</div>
+				<div class="mb-3">
+					<label class="form-label" for="npersonne">Nombre de personnes</label>
+					<input class="form-control" type="number" id="npersonne" name="nombre de personnes" min="1" max="4">
+				</div>
+				<div class="mb-3">
+					<label class="form-label" for="npetitdejeuner">Nombre de petits déjeuners</label>
+					<input class="form-control" type="number" id="npetitdejeuner" name="nombre de petits déjeuners" min="0" max="4">
+				</div>
+				<div class="mb-3 form-check">
+					<input class="form-check-input" type="checkbox" id="parking" name="parking">
+					<label class="form-check-label" for="parking">Parking</label>
+				</div>
+				<div class="mb-3">
+					<label class="form-label" for="commentaires">Message</label>
+					<textarea class="form-control" id="commentaires" placeholder="Vous pouvez nous laisser ici un message"></textarea>
+				</div>
+				<div class="mb-3 form-check">
+					<input class="form-check-input" type="checkbox" id="confidentialite" required name=" confidentialité" />
+					<label class="form-check-label" for="confidentialite">
+						En cochant cette case,vous acceptez notre
+						<router-link to="/confidentiality">Politique de confidentialité</router-link>
+					</label>
+				</div>
+				<div class="mb-3">
+					<input class="btn btn-primary" type="submit" value="Valider" />
+				</div>
+			</form>
+		</div>
     </div>
 </template>
 <script>
--- src/components/Legal.vue
+++ src/components/Legal.vue
@@ -1,40 +1,36 @@
 <template>
     <div class="container">
-        <section>
-            <titre>Mentions légales obligatoires</titre>
-            <section>
-                <titre>Raison sociale</titre>
+        <div class="m-4">
+            <h1>Mentions légales obligatoires</h1>
+            <div>
+                <h3>Raison sociale</h3>
                 <p>Auberge de Peillon, place Auguste Arnulf, Peillon</p>
                 <p>N° SIREN :123 456 789</p>
-            </section>
-            <section>
-                <titre>Directeur de la publication</titre>
-                <p><q>Au sens de l'article 93-2 de la loi n° 82-652 du 29 juillet 1982. </q></p>
+            </div>
+            <div>
+                <h3>Directeur de la publication</h3>
+                <p><q>Au sens de l'article 93-2 de la loi n° 82-652 du 29 juillet 1982.</q></p>
                 <p>M. Jean Dupont</p>
-            </section>
-            <section>
-                <titre>Traitement des données à caractère personnel</titre>
+            </div>
+            <div>
+                <h3>Traitement des données à caractère personnel</h3>
                 <p>Le site n'étant qu'un projet étudiant, aucune donnée à caractère personnel n'est stockée ou utilisée.</p>
                 <p>Aucune déclaration CNIL n'a donc été effectuée, n'étant pas nécessaire.</p>
-                <q>Conformément aux dispositions de la loi n° 78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés, vous disposez d'un droit d'accès, de modification, de rectification et de suppression des données qui vous concernent. Pour demander une modification, rectification ou suppression des données vous concernant, il vous suffit d'envoyer un courrier par voie électronique ou postale à la DILA en justifiant de votre identité.</q>
-            </section>
-        </section>
-        <section>
-            <titre>Logiciels utilisés</titre>
+                <p><q>Conformément aux dispositions de la loi n° 78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés, vous disposez d'un droit d'accès, de modification, de rectification et de suppression des données qui vous concernent. Pour demander une modification, rectification ou suppression des données vous concernant, il vous suffit d'envoyer un courrier par voie électronique ou postale à la DILA en justifiant de votre identité.</q></p>
+            </div>
             <div>
+                <h3>Logiciels utilisés</h3>
                 <p>La carte embarquée utilise la technologie <a href="https://www.openstreetmap.org">OpenStreeMap</a></p>
             </div>
-        </section>
-        <section>
-            <titre>Propriété intellectuelle</titre>
             <div>
+                <h3>Propriété intellectuelle</h3>
                 <p>L'ensemble du code source, des images, des textes et autres sont la propriété des auteurs, sauf citation expresse.</p>
             </div>
-        </section>
-        <section>
-            <titre>Contact</titre>
-            <p><a href="mailto:admin.hotel.peillon@outlook.fr">admin.hotel.peillon@outlook.fr</a></p>
-        </section>
+            <div>
+                <h3>Contact</h3>
+                <p><a href="mailto:admin.hotel.peillon@outlook.fr">admin.hotel.peillon@outlook.fr</a></p>
+            </div>
+        </div>
     </div>
 </template>
 <script>
