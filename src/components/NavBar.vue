047afdc26decb567549fc830caf574c25bda6e5f
HM22-code <H.M-22@outlook.fr>
2024-10-25 14:07:40 +0200

    Adding navbar icon

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,7 +4,10 @@
 <template>
     <nav class="navbar navbar-expand-lg bg-body-tertiary bg-primary">
         <div class="container-fluid">
-            <router-link class="navbar-brand" to="/">Auberge Peillon</router-link>
+            <router-link class="navbar-brand" to="/">
+                <img src="../assets/img/icon.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                Auberge Peillon
+            </router-link>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
