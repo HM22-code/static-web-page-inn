7a015393022ce78ab13fc321878081bae2454c8e
HM22-code <H.M-22@outlook.fr>
2024-10-25 12:03:17 +0200

    Adding parallax banner

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,23 @@
 <script setup>
+import ParalllaxBanner from './ParalllaxBanner.vue';
+
 </script>
 
 <template>
-    <div class="container">
-        <h1>Bienvenue à l'Auberge Peillon</h1>
-		<h6>Hôtel 3 étoiles - restaurant gastronomique - bistrot</h6>
-		<img src="../assets/img/auberge.jpg" class="img-fluid" alt="auberge">
-		<p>
-			Située dans l'un des plus beaux villages de la Côte d'Azur, l'auberge de Peillon vous reçoit dans son parc de 5 hectares de vergers et d'oliviers.
-			Notre table saura satisfaire les gourmets les plus exigeants. Nous privilégions les saveurs locales et de saison.
-			Vous pourrez profiter de l'espace de remise en forme qui est accessible gratuitement à tous les clients de l'hôtel.
-		</p>
+    <div>
+		<ParalllaxBanner />
+        <div>
+			<img src="../assets/img/auberge.jpg" class="img-fluid" alt="auberge">
+			<div class="container">
+				<h1>Bienvenue à l'Auberge Peillon</h1>
+                <p>Hôtel 3 étoiles - restaurant gastronomique - bistrot</p>
+				<p>
+				Située dans l'un des plus beaux villages de la Côte d'Azur, l'auberge de Peillon vous reçoit dans son parc de 5 hectares de vergers et d'oliviers.
+				Notre table saura satisfaire les gourmets les plus exigeants. Nous privilégions les saveurs locales et de saison.
+				Vous pourrez profiter de l'espace de remise en forme qui est accessible gratuitement à tous les clients de l'hôtel.
+				</p>
+			</div>
+		</div>
     </div>
 </template>
 
---
+++ src/components/ParalllaxBanner.vue
@@ -0,0 +1,44 @@
+<script setup>
+</script>
+
+<template>
+    <div class="parallax-banner">
+        <div class="parallax-background"></div>
+        <div class="parallax-content container">
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.parallax-banner {
+    position: relative;
+    height: 500px;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.parallax-background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url('../assets/img/parallax-banner.jpeg');
+    background-size: cover;
+    background-attachment: fixed;
+    background-position: center;
+    z-index: -1;
+}
+
+.parallax-content {
+    text-align: center;
+    z-index: 1;
+    color: white;
+    top: 20%;
+    text-shadow: 2px 2px 8px rgba(0, 0, 0, 0.6);
+    border-radius: 10px;
+    backdrop-filter: blur(1px);
+}
+</style>
